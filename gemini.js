f03c1452890f41f203fa464aab98f7a8058ba517
airabylimbat <airabylimbat@gmail.com>
2026-05-15 06:33:11 +0700

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -4,7 +4,7 @@ async function askGemini(prompt){
 
     const response = await fetch(
 
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
 
         {
             method:"POST",
